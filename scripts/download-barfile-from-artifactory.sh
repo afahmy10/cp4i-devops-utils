53e29649884dc0b4e3c3eb8014b6a1f77da9e5d8
afahmy10 <55480398+afahmy10@users.noreply.github.com>
2022-05-26 01:54:23 +0300

    Update download-barfile-from-artifactory.sh

--- scripts/download-barfile-from-artifactory.sh
+++ scripts/download-barfile-from-artifactory.sh
@@ -9,4 +9,4 @@ ARTIFACTORY_BAR_FILE=$4
 ARTIFACTORY_USER=$5
 ARTIFACTORY_PASSWORD=$6
 
-curl -H "X-JFrog-Art-Api:AKCp8mYy1qJ7cucUzVJLW76m6hyk8DSobcztyKDqqwnd6bHatjWrDCBRQH3iwfdqYFxDhBq1n" https://afahmy.jfrog.io/artifactory/test-generic-local/cp4i/$ARTIFACTORY_BAR_FILE --output $ARTIFACTORY_BAR_FILE
+curl -H "X-JFrog-Art-Api:AKCp8mYy1qJ7cucUzVJLW76m6hyk8DSobcztyKDqqwnd6bHatjWrDCBRQH3iwfdqYFxDhBq1n" https://afahmy.jfrog.io/ui/native/default-generic-local/cp4i/$ARTIFACTORY_BAR_FILE --output $ARTIFACTORY_BAR_FILE
