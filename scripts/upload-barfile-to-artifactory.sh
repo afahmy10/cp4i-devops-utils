a1ba24d31e1308ece2d12d0a38595964e9e047a1
afahmy10 <55480398+afahmy10@users.noreply.github.com>
2022-05-26 01:38:10 +0300

    Update upload-barfile-to-artifactory.sh

--- scripts/upload-barfile-to-artifactory.sh
+++ scripts/upload-barfile-to-artifactory.sh
@@ -9,4 +9,4 @@ ARTIFACTORY_BAR_FILE=$4
 ARTIFACTORY_USER=$5
 ARTIFACTORY_PASSWORD=$6
 CHECKSUM=`shasum -a 1 $ARTIFACTORY_BAR_FILE | awk '{ print $1 }'`
-curl -H "X-JFrog-Art-Api:AKCp8mYy1qJ7cucUzVJLW76m6hyk8DSobcztyKDqqwnd6bHatjWrDCBRQH3iwfdqYFxDhBq1n" -T $ARTIFACTORY_BAR_FILE https://afahmy.jfrog.io/artifactory/test-generic-local/$ARTIFACTORY_BAR_FILE
+curl -H "X-JFrog-Art-Api:AKCp8mYy1qJ7cucUzVJLW76m6hyk8DSobcztyKDqqwnd6bHatjWrDCBRQH3iwfdqYFxDhBq1n" -T $ARTIFACTORY_BAR_FILE https://afahmy.jfrog.io/artifactory/test-generic-local/cp4i/$ARTIFACTORY_BAR_FILE
